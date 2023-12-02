68e40e428d472df4687fba4a461cb296cc6a2486
TheRook <firealwaysworks@gmail.com>
2023-12-01 17:35:55 -0800

    working better

--- serviceworker.js
+++ serviceworker.js
@@ -1,34 +1,49 @@
 // service-worker.js
 
-self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-  
-    // Check if the request is to the local domain
-    if (url.origin === location.origin) {
-      event.respondWith(fetchWithProxy(event.request));
+async function fetchWithProxy(url) {
+  try {
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.contents;
     } else {
-      event.respondWith(fetch(event.request));
+      throw new Error('Network response was not ok.');
     }
-  });
-  
-  async function fetchWithProxy(request) {
-    try {
-      // Modify the request to use the proxy
-      const proxyURL = 'https://corsproxy.io/?'; // Replace with your actual proxy URL
-      const proxiedRequest = new Request(proxyURL, {
-        method: request.method,
-        headers: request.headers,
-        body: request.method !== 'GET' ? request.body : null,
-        mode: 'cors',
-        credentials: 'same-origin',
-        redirect: 'follow',
-      });
-  
-      const response = await fetch(proxiedRequest);
-      return response;
-    } catch (error) {
-      // Handle fetch errors
-      console.error('Fetch failed:', error);
-      return new Response(null, { status: 500, statusText: 'Fetch failed' });
+  } catch (error) {
+    if (url.startsWith(window.x_proxy_backup)) {
+      console.log("Fetching:" + window.x_proxy_backup);
+      return fetch(url);
+    } else {
+      console.log("fetching:" + window.x_proxy_backup + url);
+      return fetch(window.x_proxy_backup + url);
     }
   }
+}
+
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      // Check if the requested resource is in the cache
+      if (cachedResponse) {
+        // If found in cache, respond with the cached resource
+        return cachedResponse;
+      } else {
+        // If not found in cache, fetch from the network
+        return fetchWithProxy(event.request).then(networkResponse => {
+          // Cache the fetched response for future use
+          caches.open('browser').then(cache => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          // Return the fetched response
+          return networkResponse;
+        }).catch(error => {
+          // Handle fetch errors
+          console.error('Fetch error:', error);
+        });
+      }
+    })
+  );
+});
